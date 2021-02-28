c4495b9f67fea9d9450bce11d45360fc975091e5
shreyas <spapinwar@gmail.com>
2021-02-28 18:12:45 +0530

    Tranactions viewmodel

---
+++ Dash Monkey/Models/TxnModel.swift
@@ -0,0 +1,67 @@
+//
+//  TxnModel.swift
+//  Dash Monkey
+//
+//  Created by Shreyas Papinwar on 28/02/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct TxnModel: Codable, Hashable, Identifiable {
+    var id = UUID()
+    
+    // common stuff
+    var block_signed_at: Date
+    var tx_hash: String
+    var tx_offset: Int
+    var success: Bool
+    var from_address: String
+    var to_address: String
+    var from_address_label: String
+    var to_address_label: String
+    
+    // Token Specific
+    var contract_address: String?
+    var contract_decimals: Int?
+    var contract_in_quote: Double?
+    
+    // stores base or calculated token
+    var amount: Double
+    var amount_in_quote: Double
+    
+    // gas stuff, can do more?
+    var gas_offered: Double
+    var gas_price: Double
+    var gas_spent: Double
+    var gas_spent_quote: Double // cost of txn
+    var gas_rate_quote: Double // price of eth
+    
+    // TODO - Palash
+//    init() {
+//        self.block_signed_at = Date()
+//        self.tx_hash = ""
+//        var tx_offset = 0
+//        var success = true
+//        var from_address: String
+//        var to_address: String
+//        var from_address_label: String
+//        var to_address_label: String
+//
+//        // Token Specific
+//        var contract_address: String?
+//        var contract_decimals: Int?
+//        var contract_in_quote: Double?
+//
+//        // stores base or calculated token
+//        var amount: Double
+//        var amount_in_quote: Double
+//
+//        // gas stuff, can do more?
+//        var gas_offered: Double
+//        var gas_price: Double
+//        var gas_spent: Double
+//        var gas_spent_quote: Double // cost of txn
+//        var gas_rate_quote: Double
+//    }
+}
---
+++ Dash Monkey/ViewModels/TxnViewModel.swift
@@ -0,0 +1,148 @@
+//
+//  TxnViewModel.swift
+//  Dash Monkey
+//
+//  Created by Shreyas Papinwar on 28/02/21.
+//
+
+import Foundation
+import Combine
+import Alamofire
+
+class TxnViewModel: ObservableObject {
+    @Published var txns = [TxnModel]()
+    
+    func getAllBaseTxn(chainID:String, address:String, currency:String) {
+      let url:String = covalentAPI + "/v1/" + chainID + "/address/" + address + "/transactions_v2/"
+
+      let params:[String:Any] = [
+        "key": covalentAPIKey,
+        "quote-currency": currency.lowercased(),
+        "no-logs": true
+      ]
+
+      let req = AF.request(
+        url,
+        method: .get,
+        parameters: params
+      )
+
+      req.responseJSON { res in
+        var txns: [TxnModel] = []
+        if let json = res.value as? [String:Any] {
+          if let data = json["data"] as? [String:Any] {
+            if let items = data["items"] as? [[String:Any]] {
+                let dateformatter = DateFormatter()
+                dateformatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+              for item in items {
+                if item["to_address"] as? String != address {
+                    continue
+                }
+                // else
+                var txn = TxnModel()
+                
+                // txn stuff
+                txn.block_signed_at = dateformatter.date(from: item["block_signed_at"] as! String)!
+                txn.tx_hash = item["tx_hash"] as! String
+                txn.tx_offset = item["tx_offset"] as! Int
+                txn.success = item["successful"] as! Bool
+                txn.from_address = item["from_address"] as! String
+                txn.to_address = item["to_address"] as! String
+                txn.from_address_label = item["from_address_label"] as! String
+                txn.to_address_label = item["to_address_label"] as! String
+                
+                txn.amount = item["value"] as! Double // TODO convert wETH to ETH ;; response is String, `as Double` ok?
+                txn.amount_in_quote = item["value_quote"] as! Double
+                
+                // gas stuff
+                txn.gas_offered = item["gas_offered"] as! Double
+                txn.gas_price = item["gas_price"] as! Double
+                txn.gas_spent = item["gas_spent"] as! Double
+                txn.gas_spent_quote = item["gas_quote"] as! Double
+                txn.gas_rate_quote = item["value_quote"] as! Double
+
+                txns.append(txn)
+              }
+            }
+          }
+          else { // if json["data"] == null
+            print("Error retriving balances - \(json)")
+          }
+        }
+        self.txns.append(contentsOf: txns)
+      }
+    }
+    
+    func getTokenTransfers(chainID:String, address:String, currency:String, contract:String) {
+      let url:String = covalentAPI + "/v1/" + chainID + "/address/" + address + "/transfers_v2/"
+
+      let params:[String:Any] = [
+        "key": covalentAPIKey,
+        "quote-currency": currency.lowercased(),
+        "contract-address": contract
+      ]
+
+      let req = AF.request(
+        url,
+        method: .get,
+        parameters: params
+      )
+
+      req.responseJSON { res in
+        let txns:[TxnModel] = []
+        if let json = res.value as? [String:Any] {
+          if let data = json["data"] as? [String:Any] {
+            if let items = data["items"] as? [[String:Any]] {
+                let dateformatter = DateFormatter()
+                dateformatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+              for item in items {
+                var txn = TxnModel()
+                
+                
+                // gas stuff
+                txn.gas_offered = item["gas_offered"] as! Double
+                txn.gas_price = item["gas_price"] as! Double
+                txn.gas_spent = item["gas_spent"] as! Double
+                txn.gas_spent_quote = item["gas_quote"] as! Double
+                txn.gas_rate_quote = item["value_quote"] as! Double
+                
+                // txn stuff
+                txn.block_signed_at = dateformatter.date(from: item["block_signed_at"] as! String)
+                txn.tx_hash = item["tx_hash"] as! String
+                txn.tx_offset = item["tx_offset"] as! Int
+                txn.success = item["successful"] as! Bool
+                
+                
+                
+                var transfers = item["transfers"] as? [[String:Any]]
+                if transfers?.count ?? 2 > 1 { // DNU :: fixed with xcode
+                    print("multiple transfers in same block; currently unsuported")
+                    continue
+                }
+                
+                // else transfer info
+                txn.from_address = transfers[0]["from_address"] as String
+                txn.to_address = transfers[0]["to_address"] as String
+                txn.from_address_label = transfers[0]["from_address_label"] as? String ?? nil
+                txn.to_address_label = transfers[0]["to_address_label"] as? String ?? nil
+                
+                txn.contract_in_quote = transfers[0]["quote_rate"]
+                txn.contract_decimals = transfers[0]["contract_decimals"]
+                
+                txn.amount = transfers[0]["delta"] as Double // TODO :: divide by contract decimals ;; response is String, `as Double` ok?
+                txn.amount_in_quote = transfers[0]["delta_quote"] as Double
+                
+                txns.append(txn)
+              }
+            }
+          }
+          else { // if json["data"] == null
+            print("Error retriving balances - \(json)")
+          }
+        }
+        self.txns.append(contentsOf: txns)
+      }
+    }
+    
+    
+}
